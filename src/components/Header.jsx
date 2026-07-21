019e415bbf9ed291327089e3fde40aab047b3ead
Milad.Msr <read.miladmsr@gmail.com>
2026-07-21 10:49:13 +0330

    Create Header Section and responsive for all devices

---
+++ src/components/Header.jsx
@@ -0,0 +1,55 @@
+import { IoMenu } from "react-icons/io5";
+import { MobileMenu } from "./MobileMenu";
+import { useEffect, useState } from "react";
+import logoLight from "../assets/logo-light.svg"
+import { MdOutlineShoppingCart } from "react-icons/md";
+
+export const Header = () => {
+
+  const [toggleMenu, setToggleMenu] = useState(false)
+  
+
+  useEffect(() => {
+    document.body.classList.toggle("menu-open", toggleMenu);
+  }, [toggleMenu]);
+
+
+  return (
+    <>
+      <div className="bg-[#4f3120ff] fixed w-full flex justify-between items-center py-5 px-2 md:py-7 md:px-8">
+        <div className="flex-1">
+          <a href="#"><img src={logoLight} alt="cozynest" className="w-35 md:w-45"/></a>
+        </div>
+
+        <div className="hidden lg:flex lg:justify-center gap-15 font-montserrat font-bold text-[18px] text-white flex-2 tracking-widest uppercase">
+          <a href="#home">Home</a>
+          <a href="#home">Shop</a>
+          <a href="#home">Blog</a>
+          <a href="#home">Latest</a>
+          <a href="#home">Info</a>
+        </div>
+
+        <div className="flex-center gap-5 flex-1 flex justify-end">
+          <div className="flex-center relative after:content-['2'] after:flex after:items-center 
+          after:justify-center after:font-bold after:text-white after:size-5 after:bg-red-400 
+          after:absolute after:rounded-full after:-top-2 after:-right-2">
+            <MdOutlineShoppingCart className="text-[#d6c6ad] cursor-pointer text-[30px]"/>
+          </div>
+
+          <IoMenu onClick={() => setToggleMenu(true)} size={30} className="text-[#d6c6ad] cursor-pointer lg:hidden"/>
+        </div>
+      </div>
+
+      <MobileMenu toggleMenu={toggleMenu} setToggleMenu={setToggleMenu} />
+
+      <div 
+        onClick={() => setToggleMenu(false)} 
+        className={`
+          fixed inset-0 z-1 bg-[#000000cc] opacity-0 
+          transition delay-100 duration-500 invisible 
+          ${toggleMenu && "visible opacity-100"}
+        `}
+      ></div>
+    </>
+  )
+}
---
+++ src/components/MobileMenu.jsx
@@ -0,0 +1,58 @@
+import { useState } from "react";
+import { IoClose } from "react-icons/io5";
+import logoLight from "../assets/logo-light.svg";
+import { IoMdArrowDropright } from "react-icons/io";
+
+
+export const MobileMenu = ({toggleMenu, setToggleMenu}) => {
+
+  const [expandShop, setExpandShop] = useState(false)
+
+
+  return (
+    <div className={`
+      fixed top-0 left-0 w-80 h-screen z-3 pt-25 flex items-center flex-col 
+      -translate-x-full overflow-auto transition duration-700 bg-[#3e2c22]
+      ${toggleMenu && "translate-x-0"}
+    `}>
+
+      <div onClick={() => setToggleMenu(false)} 
+        className="absolute right-5 top-5 border p-2 rounded-full text-white cursor-pointer"
+      >
+        <IoClose size={20}/>
+      </div>
+
+      <a href="#" className="mx-auto mb-10">
+        <img src={logoLight} alt="cozynest" className="w-45"/>
+      </a>
+
+      <ul className="flex flex-col items-start w-full px-10 font-montserrat font-bold">
+        <li><a href="#home" className="nav-item">Home</a></li>
+
+        <li>
+          <a href="#home" onClick={() => setExpandShop(prev => !prev)} className="nav-item">
+            Shop
+            <IoMdArrowDropright size={20} className={`inline transition duration-400 ${expandShop && "rotate-90"}`}/>
+          </a>
+
+          <div className={`
+            flex flex-col text-[#dac3a6] border-b h-0 px-5 space-y-1 invisible 
+            ${expandShop && "visible h-46 pb-52 pt-4"}
+          `}>
+            <a href="#">All Products</a>
+            <a href="#">Clothing</a>
+            <a href="#">Candles</a>
+            <a href="#">Accessories</a>
+            <a href="#">Bags</a>
+            <a href="#">Clocks</a>
+            <a href="#">Sale</a>
+          </div>
+        </li>
+
+        <li><a href="#blog" className="nav-item">Blog</a></li>
+        <li><a href="#latest" className="nav-item">Latest</a></li>
+        <li><a href="#info" className="nav-item">Info</a></li>
+      </ul>
+    </div>
+  )
+}
